f638294205553a73e6eb3bea6666a673f5b3c9e8
Alonso Anchante Moreno <alonso.amoreno@live.com>
2023-07-21 12:37:52 -0500

    Creando cumputed property y agregand tabla de clientes

--- src/views/ClientesView.vue
+++ src/views/ClientesView.vue
@@ -1,21 +1,23 @@
 <script setup>
 import axios from "axios";
-import { onMounted } from "vue";
+import { onMounted, ref, computed } from "vue";
 import RouterLink from "../components/UI/RouterLink.vue";
 import Heading from "../components/UI/Heading.vue";
-defineProps({
-  titulo: String,
-});
+
+const clientes = ref([]);
+
 onMounted(() => {
   axios
     .get("http://localhost:4000/clientes")
-    .then(({ data }) => {
-      console.log(data);
-    })
-    .catch(({ message }) => {
-      console.log(message);
-    });
+    .then(({ data }) => (clientes.value = data))
+    .catch(({ message }) => console.log(message));
 });
+
+defineProps({
+  titulo: String,
+});
+
+const existenClientes = computed(() => clientes.value.length > 0);
 </script>
 <template>
   <div>
@@ -23,6 +25,47 @@ onMounted(() => {
       <RouterLink to="agregar-cliente">Agregar Cliente</RouterLink>
     </div>
     <Heading>{{ titulo }}</Heading>
+    <div
+      v-if="existenClientes"
+      class="flow-root mx-auto mt-10 p-5 bg-white shadow"
+    >
+      <div class="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div class="min-w-full py-2 align-middle sm:px-6 lg:px-8">
+          <table class="min-w-full divide-y divide-gray-300">
+            <thead>
+              <tr>
+                <th
+                  scope="col"
+                  class="p-2 text-left text-sm font-extrabold text-gray-600"
+                >
+                  Nombre
+                </th>
+                <th
+                  scope="col"
+                  class="p-2 text-left text-sm font-extrabold text-gray-600"
+                >
+                  Empresa
+                </th>
+                <th
+                  scope="col"
+                  class="p-2 text-left text-sm font-extrabold text-gray-600"
+                >
+                  Estado
+                </th>
+                <th
+                  scope="col"
+                  class="p-2 text-left text-sm font-extrabold text-gray-600"
+                >
+                  Acciones
+                </th>
+              </tr>
+            </thead>
+            <tbody class="divide-y divide-gray-200 bg-white"></tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+    <p v-else class="text-center mt-10">No hay Clientes</p>
   </div>
 </template>
 
